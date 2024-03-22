abe2f1369df06cb27b86a96e40d8ec15b6215a80
DanishSuffian <danishsuffian14@gmail.com>
2024-03-22 20:52:36 +0800

    Seventh Commit - Continued with the design of the landing page.

--- static/scripts/landing.js
+++ static/scripts/landing.js
@@ -47,3 +47,28 @@ ScrollReveal().reveal(".header__container .header__socials", {
     origin: "left",
     delay: 1500,
 });
+
+ScrollReveal().reveal(".about__image-1, .about__image-3", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+ScrollReveal().reveal(".about__image-2", {
+    ...scrollRevealOption,
+    origin: "left",
+});
+ScrollReveal().reveal(".about__content .section__subheader", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".about__content .section__header", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+ScrollReveal().reveal(".about__content p", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+ScrollReveal().reveal(".about__content .about__btn", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
